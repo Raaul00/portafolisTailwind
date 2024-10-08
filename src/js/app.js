f88f5840e14929b8e2ba4cf6f07f709edadb4d4d
Raul Gabriel <raul.stoianof@cirvianum.cat>
2024-10-08 21:19:42 +0200

    primer commit

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+// Afegeix interactivitat bàsica si cal.
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Pàgina carregada correctament");
+});
---
+++ webpack.config.common.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/js/app.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+};
